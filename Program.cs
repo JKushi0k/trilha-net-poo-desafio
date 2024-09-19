40d564d11432f21fa671bd38af2445ee6c4bece2
JKushu0k <asta56221@gmail.com>
2024-09-19 10:39:54 -0300

    Desafio de projeto Celular POO concluido!

--- Program.cs
+++ Program.cs
@@ -1,3 +1,14 @@
 ﻿using DesafioPOO.Models;
 
 // TODO: Realizar os testes com as classes Nokia e Iphone
+Console.WriteLine("Smartphone Nokia");
+Nokia n1 = new Nokia(numero: "123456789", modelo: "Nokia", imei: "11111111", memoria: 120);
+n1.Ligar();
+n1.InstalarAplicativo("Facebook");
+
+Console.WriteLine("-------------");
+
+Console.WriteLine("Smartphone Iphone");
+Iphone i1 = new Iphone(numero: "987654321", modelo: "Iphone", imei: "222222222", memoria: 240);
+i1.ReceberLigacao();
+i1.InstalarAplicativo("Instagram");
